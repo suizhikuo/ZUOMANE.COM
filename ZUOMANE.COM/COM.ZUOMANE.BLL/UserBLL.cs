fbe337ca39a5865c04e5ea510ad44532bf05751b
suizhikuo <suizhikuo@126.com>
2014-12-17 11:01:57 +0800

    11

---
+++ ZUOMANE.COM/COM.ZUOMANE.BLL/UserBLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COM.ZUOMANE.BLL
+{
+	public class UserBLL
+	{
+		public string[] GetRoles(string Name)
+		{
+			return new string[] { "admin","gust"};
+		}
+	}
+}
--- ZUOMANE.COM/COM.ZUOMANE.Portal/Global.asax.cs
+++ ZUOMANE.COM/COM.ZUOMANE.Portal/Global.asax.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using COM.ZUOMANE.BLL;
 
 namespace COM.ZUOMANE.Portal
 {
-	public class MvcApplication : System.Web.HttpApplication
+	public class PortalApplication : System.Web.HttpApplication
 	{
+		/// <summary>
+		/// 引用http://www.cnblogs.com/ldp615/archive/2010/10/27/asp-net-mvc-forms-authentication-roles-authorization-demo.html
+		/// </summary>
+		public PortalApplication()
+		{
+			AuthorizeRequest += PortalApplication_AuthorizeRequest;
+		}
+
+		/// <summary>
+		/// 可以使用这种标签了:
+		/// [Authorize(Users="admin", Roles = "admin")]
+		/// [Authorize(Roles = "manager")]
+		/// [Authorize(Roles = "employee,manager")]
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void PortalApplication_AuthorizeRequest(object sender, EventArgs e)
+		{
+			IIdentity id = Context.User.Identity;
+			if (id.IsAuthenticated)
+			{
+				var roles = new UserBLL().GetRoles(id.Name);
+				Context.User = new GenericPrincipal(id, roles);
+			}
+		}
+
 		protected void Application_Start()
 		{
 			AreaRegistration.RegisterAllAreas();
