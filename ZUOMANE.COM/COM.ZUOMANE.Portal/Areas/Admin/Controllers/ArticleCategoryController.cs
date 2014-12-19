0dcc0f867d448ea4406202fe6a289355e1719635
suizhikuo <suizhikuo@126.com>
2014-12-19 18:47:38 +0800

    11

---
+++ ZUOMANE.COM/COM.ZUOMANE.Portal/Areas/Admin/Controllers/ArticleCategoryController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using COM.ZUOMANE.Models;
+
+namespace COM.ZUOMANE.Portal.Areas.Admin.Controllers
+{
+	public class ArticleCategoryController : Controller
+	{
+		private BLL.ArticleCategoryBLL articleCategoryBLL = new BLL.ArticleCategoryBLL();
+
+		//
+		// GET: /ArticleCategory/
+
+		public ActionResult Index()
+		{
+			return View(articleCategoryBLL.GetAll());
+		}
+
+		//
+		// GET: /ArticleCategory/Details/5
+
+		public ActionResult Details(int id = 0)
+		{
+			ArticleCategory articlecategory = articleCategoryBLL.Find(id);
+			if (articlecategory == null)
+			{
+				return HttpNotFound();
+			}
+			return View(articlecategory);
+		}
+
+		//
+		// GET: /ArticleCategory/Create
+
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		//
+		// POST: /ArticleCategory/Create
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(ArticleCategory articlecategory)
+		{
+			if (ModelState.IsValid)
+			{
+				articleCategoryBLL.Create(articlecategory);
+
+				return RedirectToAction("Index");
+			}
+
+			return View(articlecategory);
+		}
+
+		//
+		// GET: /ArticleCategory/Edit/5
+
+		public ActionResult Edit(int id = 0)
+		{
+			ArticleCategory articlecategory = articleCategoryBLL.Find(id);
+			if (articlecategory == null)
+			{
+				return HttpNotFound();
+			}
+			return View(articlecategory);
+		}
+
+		//
+		// POST: /ArticleCategory/Edit/5
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(ArticleCategory articlecategory)
+		{
+			if (ModelState.IsValid)
+			{
+				articleCategoryBLL.Edit(articlecategory);
+				return RedirectToAction("Index");
+			}
+			return View(articlecategory);
+		}
+
+		//
+		// GET: /ArticleCategory/Delete/5
+
+		public ActionResult Delete(int id = 0)
+		{
+			ArticleCategory articlecategory = articleCategoryBLL.Find(id);
+			if (articlecategory == null)
+			{
+				return HttpNotFound();
+			}
+			return View(articlecategory);
+		}
+
+		//
+		// POST: /ArticleCategory/Delete/5
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			articleCategoryBLL.Delete(id);
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			articleCategoryBLL.Dispose();
+			base.Dispose(disposing);
+		}
+	}
+}
---
+++ ZUOMANE.COM/COM.ZUOMANE.Portal/Areas/WebAPI/WebAPIAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace COM.ZUOMANE.Portal.Areas.WebAPI
+{
+    public class WebAPIAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WebAPI";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WebAPI_default",
+                "WebAPI/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
